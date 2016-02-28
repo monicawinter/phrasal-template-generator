0ab9bb0fa221bd19f4e4e26d8e073c9c94e6f97d
monicawinter <m.m.winter@hotmail.com>
2016-02-28 14:38:30 -0500

    added more javascript

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ var adjective = prompt('An adjective (beautiful, super, etc.)');
 var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
+
+var abcStory = ['a', 'b', 'c'];
+
+switch (abcStory) {
+  case 'a' :
+    document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
+    break;
+
+  case 'b' :
+    document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+    break;
+
+  case 'c' :
+    document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.');
+    break;
+}
