007104f2bc4182a047d18a4aa32300cefcb614cc
monicawinter <m.m.winter@hotmail.com>
2016-02-28 14:23:01 -0500

    added first few prompts in javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var intro = alert('Let’s make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
