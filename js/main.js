2b23b182acd54cec5fb0abf34944b2ae8fc32a7a
monicawinter <m.m.winter@hotmail.com>
2016-02-28 14:24:35 -0500

    added 'which story' prompt

--- js/main.js
+++ js/main.js
@@ -6,4 +6,6 @@ var creature = prompt('A creature (dragon, moth, etc.)');
 
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 
-var verbed = prompt('A past tense verb (ran, burped, etc.)');
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
